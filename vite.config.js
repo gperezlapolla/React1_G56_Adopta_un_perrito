098b88b7134a75381fe420ffec869e42ecd2e661
Guillermo Perez Lapolla <141288383+gperezlapolla@users.noreply.github.com>
2025-12-18 18:17:41 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],base: '/React1_G56_Adopta_un_perrito/',
+  base: '/React1_G56_Adopta_un_perrito/',
   
 })
